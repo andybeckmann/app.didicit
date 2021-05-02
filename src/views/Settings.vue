ede328cea3e82d77421a057da14e35e15a027893
Andy Beckmann <abec19@gmail.com>
2021-05-02 17:09:20 -0400

    Remove background and box shadow from auth box

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -216,9 +216,6 @@ export default {
 .app--main-auth {
 	max-width: 550px;
 	margin: 0 auto;
-	box-shadow: 0 25px 25px rgba(0, 0, 0, 0.10);
-	background: linear-gradient(70deg, #fff, #fefefe);
-	padding: 50px;
 	position: relative;
 
 	.app--main-auth-error-message {
